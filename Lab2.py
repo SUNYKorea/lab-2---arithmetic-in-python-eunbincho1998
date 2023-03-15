73bd928d3bc52abad25b443781d80cbb454a99c5
Eunbin Cho <eunbincho1998@gmail.com>
2023-03-15 14:16:42 +0900

    Task 2 done

--- Lab2.py
+++ Lab2.py
@@ -22,20 +22,20 @@ def neg(x):
     return x * (-1) 
 
 def sqrt(x):
-    return x ** 0.5 # fill here
+    return x ** 0.5 
 
 # -------------------------------------- Task 2 -----------------------------------
 
 # TODO: Implement the quadratic formula using *only* the functions defined here.
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
-a = 1
-b = -3
+a = 2
+b = 3
 c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
-# Note: Make sure to remove the ellipsis (...) when you're writing your code
+x1 = div(add(neg(b), sqrt(sub(exp(b, 2), mult(4, mult (a, c))))) , mult(2,a)) # first root of the quadratic equation
+x2 = div(sub(neg(b), sqrt(sub(exp(b, 2), mult(4, mult (a, c))))) , mult(2,a)) # second root of the quadratic equation
+
 
 print("First root: " + str(x1))
 print("Second root: " + str(x2))
