167f2c2836eb6a01b3a44dcd0c30e7f49f639494
Eunbin Cho <eunbincho1998@gmail.com>
2023-03-15 14:43:17 +0900

    Added comments

--- Lab2.py
+++ Lab2.py
@@ -30,16 +30,16 @@ def sqrt(x):
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
 
-print("Let's find the roots of a quadratic function 'ax^2 + bx + c'")
+print("Let's find the roots of a quadratic function 'ax^2 + bx + c'") # instructions
 print()
 
-a = float(input("What is the value of a? a = "))
+a = float(input("What is the value of a? a = ")) # input values
 b = float(input("What is the value of b? b = "))
 c = float(input("What is the value of c? c = "))
 
-x1 = div(add(neg(b), sqrt(sub(exp(b, 2), mult(4, mult (a, c))))) , mult(2,a)) # first root of the quadratic equation
-x2 = div(sub(neg(b), sqrt(sub(exp(b, 2), mult(4, mult (a, c))))) , mult(2,a)) # second root of the quadratic equation
+x1 = div(add(neg(b), sqrt(sub(exp(b, 2), mult(4, mult(a, c))))) , mult(2,a)) # first root of the quadratic equation
+x2 = div(sub(neg(b), sqrt(sub(exp(b, 2), mult(4, mult(a, c))))) , mult(2,a)) # second root of the quadratic equation
 
 print()
-print("First root: " + str(x1))
+print("First root: " + str(x1)) # show results
 print("Second root: " + str(x2))
